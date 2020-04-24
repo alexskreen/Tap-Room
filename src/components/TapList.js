72fe6b291e3656fa365cc5a7673ce027e9e3a7c0
Alex Skreen <skreenalex@gmail.com>
2020-04-24 14:35:51 -0700

    update taplist component

--- src/components/TapList.js
+++ src/components/TapList.js
@@ -4,10 +4,15 @@ import Keg from "./Keg";
 function TapList() {
   return (
   <Keg 
-    brewery = "Great Notion"
+    brewery = "Great Notion Brewing"
     beer = "Over Ripe IPA"
     beerType = "NE IPA"
     ABV = "7%"/>
+  <Keg 
+    brewery = "Monkish Brewing"
+    beer = "Planets Gotta Roll"
+    beerType = "Double NE IPA"
+    ABV = "8.3%"/>
   );
 }
 
