db041e2978a66921717c9d4b45eb39bafc2c98c4
Alex Skreen <skreenalex@gmail.com>
2020-04-24 14:21:48 -0700

    update Keg component

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,17 +1,12 @@
 import React from "react";
 
-function Keg() {
-  const beer1 = "Over Ripe";
-  const beer2 = "Planets Gotta Roll";
+function Keg(props) {
   return (
     <React.Fragment>
-      <h3>3a</h3>
       <h3>
-        {beer1} and {beer2}
+        {props.brewery} - {props.beer}
       </h3>
-      <p>
-        <em>Firebase entries not saving!</em>
-      </p>
+      <p><em>{props.beerType} - {props.ABV}</em></p>
       <hr />
     </React.Fragment>
   );
