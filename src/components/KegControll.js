215596764b7760a73c6f564afd7c604938c1f38d
Alex Skreen <skreenalex@gmail.com>
2020-04-24 16:50:04 -0700

    add Keg Control component

---
+++ src/components/KegControll.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NewKegForm from "./NewKegForm";
+import KegList from "./KegList";
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default KegControl;
