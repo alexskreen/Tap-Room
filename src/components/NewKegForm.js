9cc930f3142c4ec4dd86d1042837da1646b8ff79
Alex Skreen <skreenalex@gmail.com>
2020-04-24 18:13:09 -0700

    add working form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { v4 } from 'uuid';
 
 function NewKegForm(props) {
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.brewery.value);
+    console.log(event.target.beer.value);
+    console.log(event.target.beerType.value);
+    console.log(event.target.ABV.value);
+
+
+  }
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input type="text" name="brewery" placeholder="Brewery" />
+        <input type="text" name="beer" placeholder="Beer" />
+        <input type="text" name="beerType" placeholder="Type of Beer" />
+        <input type="text" name="ABV" placeholder="ABV" />
+        <button type="submit">Add Keg</button>
+      </form>
     </React.Fragment>
   );
 }
