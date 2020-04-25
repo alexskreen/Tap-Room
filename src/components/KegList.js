784debe7b59887ea9196909b769a74ec391cc27f
Alex Skreen <skreenalex@gmail.com>
2020-04-24 18:30:38 -0700

    update kegcontroll and keglist componenets

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,43 +1,26 @@
 import React from "react";
 import Keg from "./Keg";
+import PropTypes from "prop-types";
 
-
-const masterKegList = [
-  {
-    brewery: "Great Notion Brewing",
-    beer: "Over Ripe IPA",
-    beerType: "IPA - New England",
-    ABV: "7%",
-  },
-  {
-    brewery: "Monkish Brewing",
-    beer: "Planets Gotta Roll",
-    beerType: "IPA - Double New England",
-    ABV: "8.3%",
-  },
-  {
-    brewery: "Hudson Valley Brewing",
-    beer: "Glycerin - Pineapple Peach",
-    beerType: "IPA - Sour",
-    ABV: "8%",
-  }
-];
-
-function KegList() {
+function KegList(props) {
   return (
     <React.Fragment>
       <hr/>
-      {masterKegList.map((keg, index) =>
-        <Keg 
-        brewery = {keg.brewery}
-        beer = {keg.beer}
-        beerType = {keg.beerType}
-        ABV = {keg.ABV}
-        pints = {keg.pints}
-        key = {index}/>
+      {props.kegList.map((keg, index) =>
+      <Keg 
+      brewery={keg.brewery}
+      beer={keg.beer}
+      beerType={keg.beerType}
+      ABV={keg.ABV}
+      key={index} />
       )}
     </React.Fragment>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array
+};
+
 
 export default KegList;
