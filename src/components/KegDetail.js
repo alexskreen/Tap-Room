3ff456405affe7dfcf1ab091c333acaaaf09dd02
Alex Skreen <skreenalex@gmail.com>
2020-04-25 17:33:31 -0700

    update KegDetail to allow for display of props

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,9 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function KegDetail(props) {
+  const { keg } = props;
+
   return (
     <React.Fragment>
       <h1>Keg Details</h1>
+      <h3>{keg.brewery} - {keg.beer}</h3>
+      <p><em>{keg.beerType} - {keg.ABV}</em></p>
       <hr />
     </React.Fragment>
   );
